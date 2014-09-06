ce2dd53f9ea2eb9439c10659b120238852ef3b38
Eric Dejonckheere <eric@aya.io>
2014-09-06 23:25:27 +0200

    Test: numbers and point

--- spec/natophone_spec.rb
+++ spec/natophone_spec.rb
@@ -3,24 +3,24 @@ require_relative 'helpers'
 
 describe NATOPhone::Encoder do
 
-  let(:enc) { NATOPhone::Encoder.new('Hello World.') }
+  let(:enc) { NATOPhone::Encoder.new('1.0 Hello World.') }
 
   describe "#args" do
 
     it "shows args" do
-      expect(enc.args).to eq 'Hello World.'
+      expect(enc.args).to eq '1.0 Hello World.'
     end
 
     it "shows translation" do
-      expect(enc.translate).to eq 'hotel echo lima lima oscar - whiskey oscar romeo lima delta stop'
+      expect(enc.translate).to eq 'one stop zero - hotel echo lima lima oscar - whiskey oscar romeo lima delta stop'
     end
 
     it "shows encoded result" do
-      expect(enc.encode).to eq ["hotel", "echo", "lima", "lima", "oscar", "-", "whiskey", "oscar", "romeo", "lima", "delta", "stop"]
+      expect(enc.encode).to eq ["one", "stop", "zero", "-", "hotel", "echo", "lima", "lima", "oscar", "-", "whiskey", "oscar", "romeo", "lima", "delta", "stop"]
     end
 
     it "shows the yell string" do
-      expect(enc.yell).to eq 'HOTEL ECHO LIMA LIMA OSCAR - WHISKEY OSCAR ROMEO LIMA DELTA STOP'
+      expect(enc.yell).to eq 'ONE STOP ZERO - HOTEL ECHO LIMA LIMA OSCAR - WHISKEY OSCAR ROMEO LIMA DELTA STOP'
     end
 
   end
@@ -29,24 +29,24 @@ end
 
 describe NATOPhone::Decoder do
 
-  let(:dec) { NATOPhone::Decoder.new('hotel echo lima lima oscar - whiskey oscar romeo lima delta stop') }
+  let(:dec) { NATOPhone::Decoder.new('one point zero - hotel echo lima lima oscar - whiskey oscar romeo lima delta stop') }
 
   describe "#args" do
 
     it "shows args" do
-      expect(dec.args).to eq 'hotel echo lima lima oscar - whiskey oscar romeo lima delta stop'
+      expect(dec.args).to eq 'one point zero - hotel echo lima lima oscar - whiskey oscar romeo lima delta stop'
     end
 
     it "shows translation" do
-      expect(dec.translate).to eq 'hello world.'
+      expect(dec.translate).to eq '1.0 hello world.'
     end
 
     it "shows decoded result" do
-      expect(dec.decode).to eq ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", "."]
+      expect(dec.decode).to eq ["1", ".", "0", " ", "h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", "."]
     end
 
     it "shows the yell string" do
-      expect(dec.yell).to eq 'HELLO WORLD.'
+      expect(dec.yell).to eq '1.0 HELLO WORLD.'
     end
 
   end
