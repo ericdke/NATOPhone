43c89ced9eb53cbfc5ba0575f3b68c40edc7c38f
Eric Dejonckheere <eric@aya.io>
2014-09-07 15:24:48 +0200

    DRYing the test a little bit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 # 0.0.3 
-- Map commands to shortcuts
+- Map shortcuts to commands
+- Check input type
+- More tests
 
 # 0.0.2 2014-09-07
 - Fixes
--- spec/natophone_spec.rb
+++ spec/natophone_spec.rb
@@ -7,11 +7,12 @@ describe NATOPhone::Encoder do
   let(:enc2) { NATOPhone::Encoder.new(['1.0 Hello World.']) }
 
   describe "#args" do
+    str = '1.0 Hello World.'
     it "shows args" do
-      expect(enc.args).to eq '1.0 Hello World.'
+      expect(enc.args).to eq str
     end
     it "shows args" do
-      expect(enc2.args).to eq ["1.0 Hello World."]
+      expect(enc2.args).to eq [str]
     end
   end
 
@@ -49,19 +50,23 @@ describe NATOPhone::Encoder do
   end
 
   describe "#to_json" do
+    arg = '1.0 Hello World.'
+    trans = 'one stop zero - hotel echo lima lima oscar - whiskey oscar romeo lima delta stop'
+    deco = ["one", "stop", "zero", "-", "hotel", "echo", "lima", "lima", "oscar", "-", "whiskey", "oscar", "romeo", "lima", "delta", "stop"]
+    ye = 'ONE STOP ZERO - HOTEL ECHO LIMA LIMA OSCAR - WHISKEY OSCAR ROMEO LIMA DELTA STOP'
     it "outputs json" do
       j = JSON.parse(enc.to_json)
-      expect(j['args']).to eq '1.0 Hello World.'
-      expect(j['translate']).to eq 'one stop zero - hotel echo lima lima oscar - whiskey oscar romeo lima delta stop'
-      expect(j['encode']).to eq ["one", "stop", "zero", "-", "hotel", "echo", "lima", "lima", "oscar", "-", "whiskey", "oscar", "romeo", "lima", "delta", "stop"]
-      expect(j['yell']).to eq 'ONE STOP ZERO - HOTEL ECHO LIMA LIMA OSCAR - WHISKEY OSCAR ROMEO LIMA DELTA STOP'
+      expect(j['args']).to eq arg
+      expect(j['translate']).to eq trans
+      expect(j['encode']).to eq deco
+      expect(j['yell']).to eq ye
     end
     it "outputs json" do
       j = JSON.parse(enc2.to_json)
-      expect(j['args']).to eq ['1.0 Hello World.']
-      expect(j['translate']).to eq 'one stop zero - hotel echo lima lima oscar - whiskey oscar romeo lima delta stop'
-      expect(j['encode']).to eq ["one", "stop", "zero", "-", "hotel", "echo", "lima", "lima", "oscar", "-", "whiskey", "oscar", "romeo", "lima", "delta", "stop"]
-      expect(j['yell']).to eq 'ONE STOP ZERO - HOTEL ECHO LIMA LIMA OSCAR - WHISKEY OSCAR ROMEO LIMA DELTA STOP'
+      expect(j['args']).to eq [arg]
+      expect(j['translate']).to eq trans
+      expect(j['encode']).to eq deco
+      expect(j['yell']).to eq ye
     end
   end
 
